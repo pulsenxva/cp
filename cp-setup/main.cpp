2b5b296c8f6f00c2a0711a23bc2f553e32f2ef96
raysadmoncler <k.d.filimonov@mail.ru>
2025-11-16 11:29:46 +0300

    Update main.cpp

--- cp-setup/main.cpp
+++ cp-setup/main.cpp
@@ -1,11 +1,13 @@
-// #define _GLIBCXX_DEBUG
 #include <bits/stdc++.h>
 using namespace std;
 
 typedef long long ll;
 typedef long double ld;
 typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int> > vvi;
 typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
 
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
@@ -13,81 +15,21 @@ typedef pair<int, int> pii;
 #define eb emplace_back
 #define F first
 #define S second
-#define sz(x) (int)(x).size()
+#define sz(x) (ll)(((x).size()))
 
-const ll inf = (ll)2e18;
-const int mod = 1e9;
+const ll inf = 2e18 + 5;
+const ll mod = 1e9 + 7;
+const ll mod2 = (mod + 1) / 2;
 const ld eps = 1e-9;
-vector<unordered_set<int> > G;
-// vector<int> used;
-vector<int> er;
 
-ll gcd(ll a, ll b) {
-  if (b != 0) return gcd(b, a % b);
-  else return a;
-}
-
-ll lcm(ll a, ll b) {
-  return (a / gcd(a, b)) * b;
-}
-
-void maxi(int &a, int b) {
-  a = max(a, b);
-}
-
-void add_self(int &a, int b) {
-  a += b;
-  if (a >= mod) a %= mod;
-}
-
-void write(vector<int> &v) {
-  for (auto &d: v) cin >> d;
-}
-
-void eratosfen(ll n, vector<ll> &primes) {
-  er.assign(n + 1, 0);
-  er[0] = er[1] = 1;
-  for (ll i = 2; i <= n; i++) {
-    if (!er[i]) {
-      primes.pb(i);
-      for (ll j = 2 * i; j <= n; j += i) er[j] = 1;
-    }
-  }
-}
-
-bool comp(pair<ll, ll> &a, pair<ll, ll> &b) {
-  return a.S < b.S;
-}
-
-#ifdef LOCAL
-    #define debug(x) cerr << "[DEBUG] " << #x << " = " << (x) << '\n'
-    #define debugv(v) do { \
-        cerr << "[DEBUG] " << #v << " = ["; \
-        for (auto _i = (v).begin(); _i != (v).end(); ++_i) { \
-            cerr << *_i; \
-            if (next(_i) != (v).end()) cerr << ", "; \
-        } \
-        cerr << "]\n"; \
-    } while(0)
-#else
-    #define debug(x)
-    #define debugv(v)
-#endif
-
-
-int n, k;
 void solve() {
-  cin >> n >> k;
   
-  cout << n+k << '\n';
 }
 
-
-signed main()
-{
+signed main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  solve();
 
+  solve();
 }
